dad590a61de98493ed56b48f3c6c3a9594690922
Kenny AIME <aime.kenny@gmail.com>
2016-03-22 15:25:19 +0100

    Recherche et TVA

--- src/ShopBundle/Controller/ProduitsController.php
+++ src/ShopBundle/Controller/ProduitsController.php
@@ -3,6 +3,7 @@
 namespace ShopBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use ShopBundle\Form\RechercheType;
 
 class ProduitsController extends Controller
 {
@@ -11,13 +12,16 @@ class ProduitsController extends Controller
         $em = $this->getDoctrine()->getManager();
         $produits = $em->getRepository('ShopBundle:Produits')->byCategorie($categorie);
 
+        $categorie = $em->getRepository('ShopBundle:Categories')->find($categorie);
+        if (!$categorie) throw $this->createNotFoundException('La page n\'existe pas.');
+
         return $this->render('ShopBundle:Default:produits/layout/produits.html.twig', array('produits' => $produits));
     }
 
     public function produitsAction()
     {
         $em = $this->getDoctrine()->getManager();
-        $produits = $em->getRepository('ShopBundle:Produits')->findAll();
+        $produits = $em->getRepository('ShopBundle:Produits')->findBy(array( 'disponible' => 1 ));
 
         return $this->render('ShopBundle:Default:produits/layout/produits.html.twig', array('produits' => $produits));
     }
@@ -27,6 +31,30 @@ class ProduitsController extends Controller
         $em = $this->getDoctrine()->getManager();
         $produit = $em->getRepository('ShopBundle:Produits')->find($id);
 
+        if (!$produit) throw $this->createNotFoundException('La page n\'existe pas.');
+
         return $this->render('ShopBundle:Default:produits/layout/details.html.twig', array('produit' => $produit));
+    }
+
+    public function rechercheAction()
+    {
+        $form = $this->createForm(new RechercheType());
+        return $this->render('ShopBundle:Default:recherche/modulesUsed/recherche.html.twig', array('form' => $form->createView()));
+    }
+
+    public function rechercheTraitementAction()
+    {
+        $form = $this->createForm(new RechercheType());
+
+        if ($this->get('request')->getMethod() == 'POST')
+        {
+            $form->bind($this->get('request'));
+            $em = $this->getDoctrine()->getManager();
+            $produits = $em->getRepository('ShopBundle:Produits')->recherche($form['recherche']->getData());
+        } else {
+            throw $this->createNotFoundException('La page n\'existe pas.');
+        }
+
+        return $this->render('ShopBundle:Default:produits/layout/produits.html.twig', array('produits' => $produits));
     }
 }
